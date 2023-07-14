11fbb6632decc5f3b08fd9046321d623aa2e89f3
Sergcode <shsv100@ya.ru>
2023-07-14 15:24:45 +0700

    init commit

---
+++ src/assets/js/example.js
@@ -0,0 +1,8 @@
+import './jquery.counterup.ide/jquery.counterup.ide'
+import '../../../node_modules/waypoints/lib/jquery.waypoints.min'
+import '../../../node_modules/jquery.counterup/jquery.counterup.min'
+import './wow.settings/wow.settings'
+import './jquery.cookie'
+
+
+
---
+++ src/assets/js/index.js
@@ -0,0 +1,30 @@
+/**
+ * This is your JavaScript application's entry-point.
+ *
+ * Webpack will look into this file to find references to
+ * other source files.
+ */
+/**
+ * @module jQuery
+ */
+import 'jquery';
+
+/**
+ * @module Popper.js
+ */
+import 'popper.js';
+
+/**
+ * @module Bootstrap
+ */
+import 'bootstrap';
+
+/**
+ * @module jQuery.cookie
+ */
+import 'jquery.cookie';
+
+// Load our example file
+import './example';
+
+
